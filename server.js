5eba395f4f8b16b2b467584bf68c4fdee57cd427
varun singh <pratap@arpiantech.com>
2023-10-23 09:53:06 +0530

    changes

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const http = require("http");
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 
 var httpServer = http.createServer(app);
-let PORT = process.env.PORT || 8000;
+let PORT = process.env.PORT || 3005;
 
-const server = httpServer.listen(PORT, () => {
-    console.log(`Server is up and running on ${PORT} ...`);
-    // const WebSocket = require('ws');
-})
+
+
+const server = https.createServer({
+    key: fs.readFileSync('../../etc/ssl/private.key'), // Your private key
+    cert: fs.readFileSync('../../etc/ssl/certificate.crt') // Your SSL certificate
+  }, app).listen(PORT);
+// const server = httpServer.listen(PORT, () => {
+//     console.log(`Server is up and running on ${PORT} ...`);
+//     // const WebSocket = require('ws');
+// })
