3330b12ad0f8f38043ca924cc7d27f0576731ee8
varun singh <pratap@arpiantech.com>
2023-10-31 20:24:19 +0530

    update server .js

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ let PORT = process.env.PORT || 3005;
 var httpServer = http.createServer(app)
 
 
-// app.get('/getApi',(req,res)=>{
-//   let mailBody = util.format(emailConstent.careerMessageText,"Master");
-//   console.log('mailBody::',mailBody)
-//  const mailresponse= sentEmail({
-//     to:"testing5342@gmail.com",
-//     subject: "Apply for career",
-//     mailBody:mailBody
-//   })
-//   return  res.send('Hello Guys I am authenticated')
+app.get('/getApi',(req,res)=>{
+  let mailBody = util.format(emailConstent.careerMessageText,"Master");
+  console.log('mailBody::',mailBody)
+ const mailresponse= sentEmail({
+    to:"testing5342@gmail.com",
+    subject: "Apply for career",
+    mailBody:mailBody
+  })
+  return  res.send('Hello Guys I am authenticated')
  
-// })
+})
 
 
 const server = https.createServer({
