718c75e71472ca5b56e9bfd5cc4a3f4d8353830f
Anupsingh1234 <99235875+Anupsingh1234@users.noreply.github.com>
2023-12-26 00:11:03 +0530

    PHD admission form

--- api/careermail.js
+++ api/careermail.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const {CareerMail, EnquiryMail,AdmissionForm,StudentGrievance}=require('../controller/mailController')
+const {CareerMail, EnquiryMail,AdmissionForm,StudentGrievance,PhdAdmissionForm}=require('../controller/mailController')
 const verify=require('../middlewares/verify-token')
 
 router.post('/addCareer',verify,CareerMail);
 router.post('/enquiry',verify,EnquiryMail);
 router.post('/admissionForm',verify,AdmissionForm);
 router.post('/studentGrievance',verify,StudentGrievance);
+router.post('/phdadmissionForm',verify,PhdAdmissionForm);
 
 module.exports = router;
